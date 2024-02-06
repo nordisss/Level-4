b3845cb2130e5ffc7c787e10536eabf116bc49ea
Nord <114178811+nordisss@users.noreply.github.com>
2024-02-06 04:33:11 +0100

    Add files via upload

---
+++ ft_split.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+
+static	int	ft_count_word(char const *s, char c)
+{
+	int	i;
+	int	word;
+
+	i = 0;
+	word = 0;
+	while (s && s[i])
+	{
+		if (s[i] != c)
+		{
+			word++;
+			while (s[i] != c && s[i])
+				i++;
+		}
+		else
+			i++;
+	}
+	return (word);
+}
+
+static	int	ft_size_word(char const *s, char c, int i)
+{
+	int	size;
+
+	size = 0;
+	while (s[i] != c && s[i])
+	{
+		size++;
+		i++;
+	}
+	return (size);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	int		word;
+	char	**strs;
+	int		size;
+	int		j;
+
+	i = 0;
+	j = -1;
+	word = ft_count_word(s, c);
+	strs = (char **)malloc((word + 1) * sizeof(char *));
+	if (!strs)
+		return (NULL);
+	while (++j < word)
+	{
+		while (s[i] == c)
+			i++;
+		size = ft_size_word(s, c, i);
+		strs[j] = ft_substr(s, i, size);
+		i += size;
+	}
+	strs[j] = NULL;
+	return (strs);
+}
